372f006a6c1c014a1ab470de961e920781d84c5b
AdrianaSantorio <adriana.santorio@gmail.com>
2022-01-14 16:03:17 +0100

    created html for palindrome

--- js/palindrome.js
+++ js/palindrome.js
@@ -7,3 +7,21 @@ Creare una funzione per capire se la parola inserita è palindroma
 //2- ricavare la parola scritta al contrario
 //3- confrontare le due
 //
+
+//1
+
+//* 1.1 creare html
+
+//1.2 individuare l' elemento della pagina che svolge l' azione
+
+const button = document.getElementById('button');
+
+//1.3
+
+button.addEventListener('click', function() {
+    const word = document.getElementById('word').value;
+    console.log(word);
+
+
+})
+
