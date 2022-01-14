61a247435e0be6143f96e70db2aaa3e357f7f98e
AdrianaSantorio <adriana.santorio@gmail.com>
2022-01-14 15:45:11 +0100

    initialized repository

---
+++ js/palindrome.js
@@ -0,0 +1,9 @@
+/*Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+//1 - ricevere una parola dall' utente
+//2- ricavare la parola scritta al contrario
+//3- confrontare le due
+//
